45e3df5dcdae971c283b8685aa6ebce0b3060a20
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-26 11:00:39 +0100

    sass base, i18n base

--- webface/webface/urls.py
+++ webface/webface/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+urlpatterns += [
     path('admin/', admin.site.urls),
     path('', include('docs.urls', namespace="docs"))
 ]
