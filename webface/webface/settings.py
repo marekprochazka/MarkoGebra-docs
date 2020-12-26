dfb56e8e2e59ea42aa5c96a5b688841a2fe3599c
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-26 11:56:11 +0100

    staticfiles

--- webface/webface/settings.py
+++ webface/webface/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_sass_compiler',
 
     'docs.apps.DocsConfig'
 
@@ -130,4 +129,8 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
