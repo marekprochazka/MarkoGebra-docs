ded25ee8f4bfe1fc1924979b87bbdc6d9cdfd2fb
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-27 11:28:10 +0100

    local setting, secret key secured

--- webface/webface/settings.py
+++ webface/webface/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from django.utils.translation import ugettext_lazy as _
-
+from .local_setting import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4elpmxscb3#^*_)sux2p%e886h^kv0ahjzninl$!so**p*knuv'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
