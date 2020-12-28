7cbaa2d04203acb937e7dc60a749155f39864b74
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-28 10:36:23 +0100

    #5 - (basic pages + test trans)

--- webface/docs/urls.py
+++ webface/docs/urls.py
@@ -5,5 +5,11 @@ from django.views.generic import TemplateView
 app_name = "docs"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="test.html"))
+    path('i18n', include('django.conf.urls.i18n')),
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('about/', TemplateView.as_view(template_name="about.html"), name="about"),
+    path('features/', TemplateView.as_view(template_name="features.html"), name="features"),
+    path('installation/', TemplateView.as_view(template_name="installation.html"), name="installation"),
+    path('discussion/', TemplateView.as_view(template_name="discussion.html"), name="discussion"),
+    path('how-to-use/', TemplateView.as_view(template_name="how_to_use.html"), name="how_to_use"),
 ]
