21623341f3a2aed412c1ce729c8ce8aa91429523
Marek Prochazka <m.prochazka2002@gmail.com>
2021-04-01 15:09:17 +0200

    hiding unfinished features

--- webface/docs/urls.py
+++ webface/docs/urls.py
@@ -7,7 +7,8 @@ app_name = "docs"
 
 urlpatterns = [
     path('i18n', include('django.conf.urls.i18n')),
-    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    # path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('', TemplateView.as_view(template_name="about.html"), name="home"), # TODO change to home.html after finishing
     path('about/', TemplateView.as_view(template_name="about.html"), name="about"),
     path('features/', FeaturesView.as_view(template_name="features.html"), name="features"),
     path('installation/', TemplateView.as_view(template_name="installation.html"), name="installation"),
